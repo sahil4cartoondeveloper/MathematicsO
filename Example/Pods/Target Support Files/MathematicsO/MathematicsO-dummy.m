3b134f872dde208767e48376a36671f25f55b635
sahil4cartoondeveloper <sahil4cartoondeveloper@gmail.com>
2021-10-23 10:15:03 +0530

    version 0.1.0

---
+++ Example/Pods/Target Support Files/MathematicsO/MathematicsO-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MathematicsO : NSObject
+@end
+@implementation PodsDummy_MathematicsO
+@end
---
+++ MathematicsO/Classes/MathematicsO.swift
@@ -0,0 +1,33 @@
+//
+//  MathematicsO.swift
+//  MathematicsO
+//
+//  Created by sahil vadadoriya on 23/10/21.
+//
+
+import Foundation
+
+public class MathematicsOpration {
+    
+    public init() {}
+    
+    public func sum(_ firstValue: Int,_ secondValue: Int) {
+        let sum = firstValue + secondValue
+        print("Sum is -> \(sum)")
+    }
+    
+    public func minus(_ firstValue: Int,_ secondValue: Int) {
+        let minus = firstValue - secondValue
+        print("Minus is -> \(minus)")
+    }
+    
+    public func multiplication(_ firstValue: Int,_ secondValue: Int) {
+        let multiplication = firstValue * secondValue
+        print("Multiplication is -> \(multiplication)")
+    }
+    
+    public func divide(_ firstValue: Int,_ secondValue: Int) {
+        let divide = firstValue / secondValue
+        print("Divide is -> \(divide)")
+    }
+}
